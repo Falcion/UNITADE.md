882b3a605061467cab0d2940179638871fea6d6f
Falcion <57592842+Falcion@users.noreply.github.com>
2023-08-31 22:03:47 +0300

    chores(futureprep): prepare for future instances

--- scripts/pack.py
+++ scripts/pack.py
@@ -1,9 +1,15 @@
 import os
 import zipfile
 
+<<<<<<< Updated upstream
 index_js_path = 'out/main.js'
 manifest_path = 'source/manifest.json'
 output_zip_path = 'out/plugins-output.zip'
+=======
+index_js_path = "out/main.js"
+manifest_path = "manifest.json"
+output_zip_path = "out/plugins-output.zip"
+>>>>>>> Stashed changes
 
 # Check if the required files exist
 if not os.path.exists(index_js_path):
@@ -17,8 +23,8 @@ if not os.path.exists(manifest_path):
 os.makedirs(os.path.dirname(output_zip_path), exist_ok=True)
 
 # Create a new ZIP archive
-with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-    zipf.write(index_js_path, arcname='main.js')
-    zipf.write(manifest_path, arcname='manifest.json')
+with zipfile.ZipFile(output_zip_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+    zipf.write(index_js_path, arcname="main.js")
+    zipf.write(manifest_path, arcname="manifest.json")
 
 print(f"ZIP archive created at {output_zip_path}")
