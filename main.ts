b9011496d7a0264c51402754df9b46f97942c5ee
Falcion <57592842+Falcion@users.noreply.github.com>
2024-01-10 12:45:37 +0300

    chores(publish): debug version of syntax highlighting

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
-import { App, FileSystemAdapter, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FileSystemAdapter, Notice, Plugin, PluginSettingTab, Setting, TextFileView, WorkspaceLeaf } from 'obsidian';
 import * as fs from 'fs';
 import * as path from 'path';
 
+import './styles.scss';
+
+import './lib/codemirror'
+import './mode/properties/properties'
+
 interface UnitadeSettings {
   extensions: string;
+  is_forced_on: boolean;
+  force_extensions: string;
 }
 
 const DEFAULT_SETTINGS: UnitadeSettings = {
   extensions: 'txt',
+  is_forced_on: false,
+  force_extensions: ''
 };
 
 const CONFLICTING_PLUGINS = ['txt-as-md-obsidian', 'ini-obsidian'];
@@ -15,7 +24,11 @@ const CONFLICTING_PLUGINS = ['txt-as-md-obsidian', 'ini-obsidian'];
 export default class UnitadePlugin extends Plugin {
   settings: UnitadeSettings = DEFAULT_SETTINGS;
 
+  forceOpenEnabled: boolean | undefined;
+  forceOpenExtensions: string[] | undefined;
   async onload() {
+    super.onload();
+
     // Load the saved settings when the plugin is loaded.
     await this.loadSettings();
 
@@ -23,6 +36,7 @@ export default class UnitadePlugin extends Plugin {
 
     this.addSettingTab(new UnitadeSettingsTab(this.app, this));
     this.registerOnLoadExtensions();
+    this.registerOnLoadViews();
   }
 
   checkConflicts(additional_message: string | undefined) {
@@ -61,11 +75,26 @@ export default class UnitadePlugin extends Plugin {
 
   registerOnLoadExtensions() {
     const extensions = this.settings.extensions
+    .split(',')
+    .map((ext) => ext.trim())
+    .filter((ext) => ext !== '');
+
+    this.registerExtensions(extensions, "markdown");
+  }
+
+  registerOnLoadViews() {
+    if(this.settings.is_forced_on) {
+      const forced_extensions = this.settings.force_extensions
       .split(',')
       .map((ext) => ext.trim())
       .filter((ext) => ext !== '');
 
-      this.registerExtensions(extensions, "markdown");
+      forced_extensions.forEach(ext => {
+        this.registerView(ext, (leaf: WorkspaceLeaf) => {
+          return new CustomView(leaf, ext);
+        });
+      });
+    }
   }
 }
 
@@ -95,14 +124,109 @@ class UnitadeSettingsTab extends PluginSettingTab {
           })
       );
     new Setting(containerEl)
-          .setName('Force-update extensions')
-          .setDesc('If you changed extensions array, this plugins will reregister extensions in vault, can cause exceptions, so its recommended to reload vault or plugin instead of this option.')
+          .setName('Force-update plugin')
+          .setDesc('If you changed extensions array or any related on-load parameter, this plugin will imitate on-load process, can cause exceptions, so its recommended to reload vault instead of this option.')
           .addButton((bt) =>
             bt
               .setButtonText('Update extensions')
               .onClick(() => {
                 this.plugin.registerOnLoadExtensions();
+                this.plugin.registerOnLoadViews();
               })
           );
+          new Setting(containerEl)
+          .setName('Force-Open Files in Obsidian?')
+          .setDesc('Enable to force-open specified file extensions in Obsidian, if it is false, extensions will be opened with default editing software assigned to this extension or by themselves.')
+          .addToggle((toggle) =>
+            toggle
+              .setValue(this.plugin.settings.is_forced_on)
+              .onChange(async (value) => {
+                this.plugin.settings.is_forced_on = value;
+                await this.plugin.saveSettings();
+              })
+          );
+
+        new Setting(containerEl)
+          .setName('Force-Open Extensions')
+          .setDesc('Enter file extensions to force-open in Obsidian, separated by commas.')
+          .addText((text) =>
+            text
+              .setValue(this.plugin.settings.force_extensions)
+              .onChange(async (value) => {
+                this.plugin.settings.force_extensions = value;
+                await this.plugin.saveSettings();
+              })
+          );
+  }
+}
+
+/// customized code from ini-obsidian by @deathau
+
+class CustomView extends TextFileView {
+  codeMirror: CodeMirror.Editor;
+
+  FileExtension: string = "";
+
+  constructor(leaf: WorkspaceLeaf, extension: string) {
+    super(leaf);
+
+    this.FileExtension = extension;
+
+    // create code mirror instance
+    // @ts-ignore-error
+    this.codeMirror = CodeMirror(this.containerEl, {
+      theme: "obsidian"
+    });
+    // register the changes event
+    this.codeMirror.on('changes', this.changed);
+  }
+
+  // when the view is resized, refresh CodeMirror (thanks Licat!)
+  onResize() {
+    this.codeMirror.refresh();
+  }
+
+  // called on code mirror changes
+  changed = async (instance: CodeMirror.Editor, changes: CodeMirror.EditorChange[]) => {
+    // request a debounced save in 2 seconds from now
+    this.requestSave();
+  }
+
+  // get the new file contents
+  getViewData = () => {
+    return this.codeMirror.getValue();
+  }
+
+  // set the file contents
+  setViewData = (data: string, clear: boolean) => {
+    if (clear) {
+      // @ts-ignore-error
+      this.codeMirror.swapDoc(CodeMirror.Doc(data, "text/x-ini"))
+    }
+    else {
+      this.codeMirror.setValue(data);
+    }
+  }
+
+  // clear the view content
+  clear = () => {
+    this.codeMirror.setValue('');
+    this.codeMirror.clearHistory();
+  }
+
+  // gets the title of the document
+  getDisplayText() {
+    if (this.file) return this.file.basename;
+    else return "no file";
+  }
+
+  // confirms this view can accept ini extension
+  canAcceptExtension(extension: string) {
+    return extension == this.FileExtension;
+  }
+
+  // the view type name
+  getViewType() {
+    return this.FileExtension;
   }
 }
