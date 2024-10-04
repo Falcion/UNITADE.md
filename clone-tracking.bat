8bf5e0fef1c6ae4dc23a4b039f8fa10f7e52ea45
Falcion <57592842+Falcion@users.noreply.github.com>
2024-10-04 14:57:22 +0300

    fix(build): fix typescript shenanigans and implement clone .SH script for UNIX-systems

--- clone-tracking.bat
+++ clone-tracking.bat
@@ -1,22 +1,46 @@
 @echo off
 
-SET "FILE_PATH=clone.bat"
+SET "CLONEBAT=clone.bat"
 
-FOR /F "tokens=*" %%i IN ('git ls-files -v %FILE_PATH%') DO SET "CURRENT_STATUS=%%i"
+FOR /F "tokens=*" %%i IN ('git ls-files -v %CLONEBAT%') DO SET "CURRENT_STATUS=%%i"
 
 IF "%CURRENT_STATUS:~0,1%"=="S" (
-    ECHO Currently, the file %FILE_PATH% is skipped.
+    ECHO Currently, the file %CLONEBAT% is skipped.
     ECHO Turning file tracking back ON...
-    git update-index --no-skip-worktree "%FILE_PATH%"
+    git update-index --no-skip-worktree "%CLONEBAT%"
     IF ERRORLEVEL 0 (
         ECHO File tracking turned on successfully.
     ) ELSE (
         ECHO Error: Could not turn file tracking on.
     )
 ) ELSE (
-    ECHO Currently, the file %FILE_PATH% is being tracked.
+    ECHO Currently, the file %CLONEBAT% is being tracked.
     ECHO Turning file tracking OFF...
-    git update-index --skip-worktree "%FILE_PATH%"
+    git update-index --skip-worktree "%CLONEBAT%"
+    IF ERRORLEVEL 0 (
+        ECHO File tracking turned off successfully.
+    ) ELSE (
+        ECHO Error: Could not turn file tracking off.
+    )
+)
+
+SET "CLONESH=clone.sh"
+
+FOR /F "tokens=*" %%i IN ('git ls-files -v %CLONESH%') DO SET "CURRENT_STATUS=%%i"
+
+IF "%CURRENT_STATUS:~0,1%"=="S" (
+    ECHO Currently, the file %CLONESH% is skipped.
+    ECHO Turning file tracking back ON...
+    git update-index --no-skip-worktree "%CLONESH%"
+    IF ERRORLEVEL 0 (
+        ECHO File tracking turned on successfully.
+    ) ELSE (
+        ECHO Error: Could not turn file tracking on.
+    )
+) ELSE (
+    ECHO Currently, the file %CLONESH% is being tracked.
+    ECHO Turning file tracking OFF...
+    git update-index --skip-worktree "%CLONESH%"
     IF ERRORLEVEL 0 (
         ECHO File tracking turned off successfully.
     ) ELSE (
