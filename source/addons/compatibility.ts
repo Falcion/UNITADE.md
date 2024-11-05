9fcd0f343b9b17a2b15a54486543942793205451
Falcion <57592842+Falcion@users.noreply.github.com>
2024-11-05 16:56:02 +0300

    chore(compatibility): hidden release patch for ECMAs+2015

--- source/addons/compatibility.ts
+++ source/addons/compatibility.ts
@@ -34,27 +34,27 @@ export default class CompatibilityModule {
     private async convert(data: any, version: string): Promise<boolean> {
         const settingsMap: { [key: string]: any } = {
             '1.*': {
-                extensions: data.extensions?.replace(',', '>') ?? '',
-                forced_extensions: data.forced_extensions?.replace(',', '>') ?? '',
+                extensions: data.extensions?.replaceAll(',', '>') ?? '',
+                forced_extensions: data.forced_extensions?.replaceAll(',', '>') ?? '',
                 is_onload: Boolean(data.is_dynamic_on),
             },
             '2.0': {
-                extensions: data.extensions?.replace(',', '>') ?? '',
-                grouped_extensions: data.grouped_extensions?.replace(',', '>') ?? '',
+                extensions: data.extensions?.replaceAll(',', '>') ?? '',
+                grouped_extensions: data.grouped_extensions?.replaceAll(',', '>') ?? '',
             },
             '2.1': {
-                grouped_extensions: data.grouped_extensions?.replace(';', '>') ?? '',
-                extensions: data.extensions?.replace(';', '>') ?? '',
-                forced_extensions: data.forced_extensions?.replace(';', '>') ?? '',
-                ignore_extensions: data.ignore_extensions?.replace(';', '>') ?? '',
-                ignore_masks: data.ignore_masks?.replace(';', '>') ?? '',
+                grouped_extensions: data.grouped_extensions?.replaceAll(';', '>') ?? '',
+                extensions: data.extensions?.replaceAll(';', '>') ?? '',
+                forced_extensions: data.forced_extensions?.replaceAll(';', '>') ?? '',
+                ignore_extensions: data.ignore_extensions?.replaceAll(';', '>') ?? '',
+                ignore_masks: data.ignore_masks?.replaceAll(';', '>') ?? '',
             },
             '2.4': {
-                grouped_extensions: data.grouped_extensions?.replace(',', '>') ?? '',
-                extensions: data.extensions?.replace(';', '>') ?? '',
-                forced_extensions: data.forced_extensions?.replace(';', '>') ?? '',
-                ignore_extensions: data.ignore_extensions?.replace(';', '>') ?? '',
-                ignore_masks: data.ignore_masks?.replace(';', '>') ?? '',
+                grouped_extensions: data.grouped_extensions?.replaceAll(',', '>') ?? '',
+                extensions: data.extensions?.replaceAll(';', '>') ?? '',
+                forced_extensions: data.forced_extensions?.replaceAll(';', '>') ?? '',
+                ignore_extensions: data.ignore_extensions?.replaceAll(';', '>') ?? '',
+                ignore_masks: data.ignore_masks?.replaceAll(';', '>') ?? '',
             },
         };
 
