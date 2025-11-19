9cb107411672a2479b09e7413f545198a6774320
Falcion <57592842+Falcion@users.noreply.github.com>
2025-11-19 11:52:05 +0300

    build(settings): create factory for toggle

---
+++ source/settings/tabs/factory/ui/factory-toggle.ts
@@ -0,0 +1,32 @@
+import { Setting } from "obsidian";
+import { ISettings } from "@settings/defaults_interface";
+import { IUnitadeTabBuilder } from "@settings/tabs/factory/builder";
+import { getDeep } from "@settings/utils/functions/deep";
+import { NestedKey } from "@settings/utils/types/nested_key";
+
+export function makeToggleSetting(
+    name: string,
+    desc: string,
+    key: NestedKey<ISettings>,
+    builder: IUnitadeTabBuilder,
+    onChangeExtra?: (value: boolean) => void | Promise<void>
+): Setting {
+    const setting = new Setting(builder.containerEl)
+        .setName(name)
+        .setDesc(desc)
+        .addToggle((toggle) => {
+            const curr = Boolean(getDeep(builder.plugin.settings, key));
+
+            toggle
+                .setValue(curr)
+                .onChange(async (value) => {
+                    await builder.updateSetting(key, value);
+
+                    if (onChangeExtra) await onChangeExtra(value);
+                });
+
+            return toggle;
+        });
+
+    return setting;
+}
