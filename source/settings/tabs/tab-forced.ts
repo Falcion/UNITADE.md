7d2319b5db2b355f3dc885c0805d8b1794a4a459
Falcion <57592842+Falcion@users.noreply.github.com>
2025-12-20 12:21:39 +0300

    chore: export blanks for correct build

---
+++ source/settings/tabs/tab-forced.ts
@@ -0,0 +1,37 @@
+import { NestedSetting } from "@settings/utils/types/nested_setting";
+import UnitadeTabGenericBuilder from "@settings/tabs/factory/builder-generic";
+import { IUnitadeTab } from "@settings/tabs/tab";
+import { Addons } from "@settings/utils/types/addons";
+import UnitadePlugin from "@main";
+
+export default class UnitadeTabForced implements IUnitadeTab {
+    tabContainer!: HTMLElement;
+    tabBuilder!: UnitadeTabGenericBuilder;
+    tabSettings!: NestedSetting[];
+
+    constructor(containerEl: HTMLElement) {
+        this.tabContainer = containerEl.createEl('div', {
+            cls: 'unitade-settings-tab-container'
+        });
+    }
+
+    display(plugin: UnitadePlugin): void {
+        this.tabBuilder = new UnitadeTabGenericBuilder(this.tabContainer, plugin);
+        this.tabSettings = [
+            this.tabBuilder.SETTING_MARKDOWN_OVERCHARGE,
+            this.tabBuilder.SETTING_CONFIG_EXTENSIONS_DEFAULT,
+            this.tabBuilder.SETTING_CONFIG_EXTENSIONS_DEFAULT_INPUT,
+            this.tabBuilder.SETTING_CONFIG_EXTENSIONS_MOBILE,
+            this.tabBuilder.SETTING_CONFIG_EXTENSIONS_MOBILE_INPUT,
+            this.tabBuilder.SETTING_CASE_INSENSITIVE
+        ];
+
+        this.tabBuilder.updateDisplays();
+        this.tabBuilder.attachAddon(this.tabBuilder.SETTING_CONFIG_EXTENSIONS_DEFAULT, Addons.Warning, 'EXTENSIONS WARNING');
+
+    }
+
+    addEventListener(type: keyof HTMLElementEventMap, listener: (this: HTMLElement, ev: Event) => any, options?: boolean | AddEventListenerOptions): void {
+        this.tabContainer.addEventListener(type, listener, options);
+    }
+}
