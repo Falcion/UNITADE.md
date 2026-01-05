727ae5c29f4aaba0d96e3c4a70484d18f6042110
Falcion <57592842+Falcion@users.noreply.github.com>
2026-01-05 21:29:38 +0300

    fix(cwe): remove prototype-polluting function

--- source/settings/utils/functions/deep.ts
+++ source/settings/utils/functions/deep.ts
@@ -5,14 +5,17 @@ export function getDeep(obj: any, path: string): any {
 
 export function setDeep<T = any>(obj: T, path: string, value: any): T {
     const parts = path.split('.');
+
     let cursor = obj as any;
+
     for (let i = 0; i < parts.length - 1; i++) {
         const p = parts[i];
-        if (!Object.prototype.hasOwnProperty.call(cursor, p) || typeof cursor[p] !== 'object') {
+        if (!Object.prototype.hasOwnProperty.call(cursor, p) || typeof cursor[p] !== 'object')
             cursor[p] = {};
-        }
         cursor = cursor[p];
     }
-    cursor[parts[parts.length - 1]] = value;
+    if (!Object.prototype.hasOwnProperty.call(cursor, parts[parts.length - 1]) || typeof cursor[parts[parts.length - 1]] !== 'object')
+        cursor[parts[parts.length - 1]] = value;
+
     return obj;
 }
