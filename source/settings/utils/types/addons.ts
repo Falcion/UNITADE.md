f644505abcc6c969888a178d1aa7b3f2ed68bcfb
Falcion <57592842+Falcion@users.noreply.github.com>
2026-01-07 03:50:50 +0300

    fix(styles): fix styling issue with addons

--- source/settings/utils/types/addons.ts
+++ source/settings/utils/types/addons.ts
@@ -1,6 +1,26 @@
 export const Addons = {
+    /**
+     * @this 
+     * Warning: 
+     * defined by yellow coloring.
+     */
     WARNING: 'warning',
+    /**
+     * @this
+     * Comment:
+     * defined by green coloring.
+     */
     COMMENT: 'comment',
+    /**
+     * @this
+     * Info:
+     * defined by default coloring with bold font.
+     */
     INFO: 'info',
+    /**
+     * @this
+     * Attention:
+     * defined by red coloring.
+     */
     ATTENTION: 'attention'
 }
