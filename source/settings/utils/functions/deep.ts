3b4d036ae3012a06bf368a2d80fdf1897c103739
Falcion <57592842+Falcion@users.noreply.github.com>
2026-01-07 04:04:29 +0300

    chore(cwe): another attempt for prototype-pollutioning
    Use "create" for intermediate objects
    to avoid inheriting from prototype.

--- source/settings/utils/functions/deep.ts
+++ source/settings/utils/functions/deep.ts
@@ -5,13 +5,11 @@ export function getDeep(obj: any, path: string): any {
 
 export function setDeep<T = any>(obj: T, path: string, value: any): T {
     const parts = path.split('.');
-
     let cursor = obj as any;
-
     for (let i = 0; i < parts.length - 1; i++) {
         const p = parts[i];
         if (!Object.prototype.hasOwnProperty.call(cursor, p) || typeof cursor[p] !== 'object')
-            cursor[p] = {};
+            cursor[p] = Object.create(null);
         cursor = cursor[p];
     }
     if (!Object.prototype.hasOwnProperty.call(cursor, parts[parts.length - 1]) || typeof cursor[parts[parts.length - 1]] !== 'object')
