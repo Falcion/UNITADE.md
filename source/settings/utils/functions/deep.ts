256f385002ea3fda1a8044306dab80860ab3d3b1
Falcion <57592842+Falcion@users.noreply.github.com>
2026-01-14 08:44:52 +0300

    chore: another prototype pollution attempt fix

--- source/settings/utils/functions/deep.ts
+++ source/settings/utils/functions/deep.ts
@@ -8,10 +8,13 @@ export function setDeep<T = any>(obj: T, path: string, value: any): T {
     let cursor = obj as any;
     for (let i = 0; i < parts.length - 1; i++) {
         const p = parts[i];
+        if (p === "__proto__" || p === "constructor") continue;
         if (!Object.prototype.hasOwnProperty.call(cursor, p) || typeof cursor[p] !== 'object')
+            /* Exclude prototype pollution by design */
             cursor[p] = Object.create(null);
         cursor = cursor[p];
     }
+    if (parts[parts.length - 1] === "__proto__" || parts[parts.length - 1] === "constructor") return obj;
     if (!Object.prototype.hasOwnProperty.call(cursor, parts[parts.length - 1]) || typeof cursor[parts[parts.length - 1]] !== 'object')
         cursor[parts[parts.length - 1]] = value;
 
